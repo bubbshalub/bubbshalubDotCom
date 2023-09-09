43489fa09c26e3d080e4daad1f50281754d2729e
vulbyte <insertanimationnamehere@gmail.com>
2023-09-09 02:12:33 -0700

    done the most basic of the seleton for this site. added a header and basic theming

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,39 @@
+<script>
+    import vulbytesWordmark from '$lib/vulbyteWordmark.svg';
+
+    const navBarLocations = new Map([
+        ["home", "/"],
+        ['links', '/links'],
+        ['filler', '/']
+    ]);
+</script>
+
+<nav>
+    <div>
+        <img
+            src={vulbytesWordmark}
+           alt="vulbytesWordmark"
+        />
+    </div>
+    {#each Array.from(navBarLocations) as [key, value] (key)}
+        <div> <a href="{value}">{key}</a></div>
+    {/each} 
+</nav>
+
+<style>
+    nav {
+        background-color: #222;
+        display: grid;
+            grid-template-columns: repeat(auto-fit, minmax(10rem, 1fr));
+        padding-bottom: 1rem;
+        padding-top: 1rem;
+    }
+    nav>div {
+        min-height: 1rem;
+        min-width: 10rem;
+        margin: auto;
+    }
+
+</style>
+
+<slot />
